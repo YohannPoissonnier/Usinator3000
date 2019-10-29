2793268940513587017893a8cb373c4f670e4c0c
YohannPoissonnier <yohann.poissonnier@gmail.com>
2019-10-29 10:57:21 -0400

    test de truc

--- src/Surveillance/Obsrv_GobelinZone.java
+++ src/Surveillance/Obsrv_GobelinZone.java
@@ -7,6 +7,13 @@ public class Obsrv_GobelinZone{
 		
 	}
 	public void alarme(Gobelin f) {	
-		
+		switch(f.getZone()) {
+		case USINE: f.modifierEtatGobelin(null);break;
+		case TEST: break;
+		case ANTRE: break;
+		case FOSSE: break;
+		default:
+			break; 
+		}
 	}
 }
